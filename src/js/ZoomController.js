b6a6a4ab6913db75f58039b67e015e3565ba0de3
nielinjie <nielinjie@gmail.com>
2013-05-31 15:48:36 +0800

    scale performance fixed.

--- src/js/ZoomController.js
+++ src/js/ZoomController.js
@@ -1,111 +1,116 @@
 /**
  * Creates a new ZoomController. Object that controls the zoom setting.
- * 
+ *
  * @constructor
  * @param {mindmaps.EventBus} eventBus
  */
-mindmaps.ZoomController = function(eventBus, commandRegistry) {
-  var self = this;
+mindmaps.ZoomController = function (eventBus, commandRegistry) {
+    var self = this;
 
-  /**
-   * @constant
-   */
-  this.ZOOM_STEP = 0.25;
+    /**
+     * @constant
+     */
+    this.ZOOM_STEP = 0.25;
 
-  /**
-   * @constant
-   */
-  this.MAX_ZOOM = 3;
+    /**
+     * @constant
+     */
+    this.MAX_ZOOM = 3;
 
-  /**
-   * @constant
-   */
-  this.MIN_ZOOM = 0.25;
+    /**
+     * @constant
+     */
+    this.MIN_ZOOM = 0.25;
 
-  /**
-   * @constant
-   */
-  this.DEFAULT_ZOOM = 1;
+    /**
+     * @constant
+     */
+    this.DEFAULT_ZOOM = 1;
 
-  /**
-   * @private
-   */
-  this.zoomFactor = this.DEFAULT_ZOOM;
+    /**
+     * @private
+     */
+    this.zoomFactor = this.DEFAULT_ZOOM;
 
-  /**
-   * Sets the zoom factor if param is within MIN_ZOOM and MAX_ZOOM bounds.
-   * 
-   * @param {Number} factor
-   */
-  this.zoomTo = function(factor) {
-    if (factor <= this.MAX_ZOOM && factor >= this.MIN_ZOOM) {
-      this.zoomFactor = factor;
-      eventBus.publish(mindmaps.Event.ZOOM_CHANGED, factor);
+    /**
+     * Sets the zoom factor if param is within MIN_ZOOM and MAX_ZOOM bounds.
+     *
+     * @param {Number} factor
+     */
+    this.zoomTo = function (factor) {
+        this.zoomFactor = factor;
+        eventBus.publish(mindmaps.Event.ZOOM_CHANGED, factor, true);
+
+    };
+
+    /**
+     * Zooms in by ZOOM_STEP.
+     *
+     * @returns {Number} the new zoomFactor.
+     */
+    this.zoomIn = function (step) {
+        step = step || this.ZOOM_STEP;
+        this.zoomFactor += step
+        if (this.zoomFactor > this.MAX_ZOOM) {
+            this.zoomFactor -= step
+        } else {
+            eventBus.publish(mindmaps.Event.ZOOM_CHANGED, this.zoomFactor, false);
+        }
+        this.arrangeRedraw()
+        return this.zoomFactor;
+    };
+
+    /**
+     * Zooms out by ZOOM_STEP,
+     *
+     * @returns {Number} the new zoomFactor.
+     */
+    this.zoomOut = function (step) {
+        step = step || this.ZOOM_STEP;
+        this.zoomFactor -= step;
+        if (this.zoomFactor < this.MIN_ZOOM) {
+            this.zoomFactor += step;
+        } else {
+            eventBus.publish(mindmaps.Event.ZOOM_CHANGED, this.zoomFactor, false);
+        }
+        this.arrangeRedraw()
+        return this.zoomFactor;
+    };
+
+
+    this.zoomByScale = function (scale) {
+        scale = scale || 1
+
+        this.zoomFactor *= scale;
+        if (this.zoomFactor < this.MIN_ZOOM) {
+            this.zoomFactor = this.MIN_ZOOM;
+        } else if (this.zoomFactor > this.MAX_ZOOM) {
+            this.zoomFactor = this.MAX_ZOOM;
+        }
+        eventBus.publish(mindmaps.Event.ZOOM_CHANGED, this.zoomFactor, false);
+        this.arrangeRedraw()
+        return this.zoomFactor;
     }
-  };
 
-  /**
-   * Zooms in by ZOOM_STEP.
-   * 
-   * @returns {Number} the new zoomFactor.
-   */
-  this.zoomIn = function(step) {
-      step=step || this.ZOOM_STEP;
-    this.zoomFactor += step
-    if (this.zoomFactor > this.MAX_ZOOM) {
-      this.zoomFactor -= step
-    } else {
-      eventBus.publish(mindmaps.Event.ZOOM_CHANGED, this.zoomFactor);
+    this.arrangeRedraw = function () {
+        window.clearTimeout(this.scaling)
+        this.scaling = window.setTimeout(function () {
+            self.zoomTo(self.zoomFactor)
+        }, 300)
     }
 
-    return this.zoomFactor;
-  };
-
-  /**
-   * Zooms out by ZOOM_STEP,
-   * 
-   * @returns {Number} the new zoomFactor.
-   */
-  this.zoomOut = function(step) {
-      step=step || this.ZOOM_STEP;
-    this.zoomFactor -= step;
-    if (this.zoomFactor < this.MIN_ZOOM) {
-      this.zoomFactor += step;
-    } else {
-      eventBus.publish(mindmaps.Event.ZOOM_CHANGED, this.zoomFactor);
+    this.zoomToOne = function () {
+        this.zoomFactor = 1
+        eventBus.publish(mindmaps.Event.ZOOM_CHANGED, this.zoomFactor, true);
+        return this.zoomFactor;
     }
 
-    return this.zoomFactor;
-  };
-
-
-  this.zoomByScale=function(scale){
-    scale = scale || 1
-
-    this.zoomFactor *= scale;
-    if (this.zoomFactor < this.MIN_ZOOM) {
-      this.zoomFactor =this.MIN_ZOOM;
-    } else if(this.zoomFactor>this.MAX_ZOOM){
-      this.zoomFactor=this.MAX_ZOOM;
-    }
-    eventBus.publish(mindmaps.Event.ZOOM_CHANGED, this.zoomFactor);
-    
-
-    return this.zoomFactor;
-  }
-
-  this.zoomToOne=function(){
-    this.zoomFactor=1
-    eventBus.publish(mindmaps.Event.ZOOM_CHANGED, this.zoomFactor);
-    return this.zoomFactor;
-  }
-
-  /**
-   * Reset zoom factor when document was closed.
-   * 
-   * @ignore
-   */
-  eventBus.subscribe(mindmaps.Event.DOCUMENT_CLOSED, function(doc) {
-    self.zoomTo(self.DEFAULT_ZOOM);
-  });
+    /**
+     * Reset zoom factor when document was closed.
+     *
+     * @ignore
+     */
+    eventBus.subscribe(mindmaps.Event.DOCUMENT_CLOSED, function (doc) {
+        self.zoomTo(self.DEFAULT_ZOOM);
+    });
 };
